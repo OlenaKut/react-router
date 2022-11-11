2b1f075e8982a1461d00cd764bfb256b6b58f7d2
OlenaKut <105447315+OlenaKut@users.noreply.github.com>
2022-11-11 20:00:25 +0100

    Added pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './style.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Home.js';
+import Home from './pages/Home.js';
+import About from './pages/About.js';
+import Contacts from './pages/Contacts.js';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
+        <Route index element={<Home />}></Route>
+        <Route element={<About />}></Route>
+        <Route element={<Contacts />}></Route>
         <Route></Route>
       </Routes>
     </BrowserRouter>

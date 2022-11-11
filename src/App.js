f3e8490853c021c66e0ec5ec4aec3e8b9a525e8e
OlenaKut <105447315+OlenaKut@users.noreply.github.com>
2022-11-11 19:54:07 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home.js';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
